06573f128321de6c9246cba9dbc5fe99c97d124f
Zetelias <130755760+Zetelias@users.noreply.github.com>
2023-11-24 19:30:02 +0100

    Main Menu

---
+++ src/Menus/MainMenu.cs
@@ -0,0 +1,33 @@
+using Godot;
+using System;
+
+public partial class MainMenu : Control
+{
+	private void _on_play_button_pressed()
+	{
+		GetTree().ChangeSceneToFile("res://GameScene.tscn");
+	}
+
+
+	private void _on_sound_button_pressed()
+	{
+		GetNode<VSlider>("CenterContainer/HBoxContainer/SoundSlider").Visible = !GetNode<VSlider>("CenterContainer/HBoxContainer/SoundSlider").Visible;
+	}
+
+	private void _on_sound_slider_value_changed(double value)
+	{
+		GetNode<AudioStreamPlayer2D>("CenterContainer/HBoxContainer/SampleSound").Play();
+		AudioServer.SetBusVolumeDb(AudioServer.GetBusIndex("Master"), (float)value);
+	}
+
+
+
+	private void _on_exit_button_pressed()
+	{
+		GetTree().Quit();
+	}
+
+}
+
+
+
