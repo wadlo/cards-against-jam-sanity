a2e4cd1746a81f6d85bcf6afed3b7b2b9759f847
wadlo <jalen.morgan000@gmail.com>
2023-11-25 18:58:31 -0500

    Camera pans and zooms in on characters

--- src/Dialogue/DialogueState.cs
+++ src/Dialogue/DialogueState.cs
@@ -3,10 +3,38 @@ using Godot;
 
 public partial class DialogueState : Node
 {
+    [Export]
+    public Node2D dwarfCameraCenter;
+
+    [Export]
+    public Node2D elfCameraCenter;
+
+    [Export]
+    public Node2D golemCameraCenter;
+
+    [Export]
+    public Camera2D camera;
+
+    [Export]
+    public float zoomedOutZoom = 0.63f;
+
+    [Export]
+    public float zoomedInZoom = 1.0f;
+
     public static Dialogue.players currentSpeaking = Dialogue.players.None;
 
     public static Dialogue currentDialogue;
     public static int currentDialogueIndex = 0;
+
+    Vector2 startCameraPosition;
+
+    Tween positionTween;
+    Tween tween;
+
+    public override void _Ready()
+    {
+        startCameraPosition = camera.GlobalPosition;
+    }
 
     public static void Next()
     {
@@ -36,6 +64,38 @@ public partial class DialogueState : Node
             return currentDialogue.dialogueLines[currentDialogueIndex].line;
         }
         return "";
+    }
+
+    public override void _Process(double delta)
+    {
+        Vector2 cameraPosition = startCameraPosition;
+        float cameraScale = zoomedInZoom;
+
+        if (GetCurrentCharacter() == Dialogue.players.None)
+        {
+            cameraScale = zoomedOutZoom;
+        }
+        else
+        {
+            cameraScale = zoomedInZoom;
+        }
+
+        if (GetCurrentCharacter() == Dialogue.players.Dwarf)
+        {
+            cameraPosition = dwarfCameraCenter.GlobalPosition;
+        }
+        if (GetCurrentCharacter() == Dialogue.players.Elf)
+        {
+            cameraPosition = elfCameraCenter.GlobalPosition;
+        }
+        if (GetCurrentCharacter() == Dialogue.players.Golem)
+        {
+            cameraPosition = golemCameraCenter.GlobalPosition;
+        }
+        tween = GetTree().CreateTween();
+
+        tween.TweenProperty(camera, "zoom", new Vector2(cameraScale, cameraScale), 0.1f);
+        tween.TweenProperty(camera, "global_position", cameraPosition, 0.1f);
     }
 
     public static Dialogue.players GetCurrentCharacter()
