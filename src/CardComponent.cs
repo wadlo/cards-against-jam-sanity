283cac10a5308432c4c37ebc09ee080e9df0a60c
illogicalapple <80916557+illogicalapple@users.noreply.github.com>
2023-11-26 08:36:38 -0600

    fullscreen

--- src/CardComponent.cs
+++ src/CardComponent.cs
@@ -4,131 +4,131 @@ using System.Collections.Generic;
 
 public partial class CardComponent : Area2D
 {
-    Vector2 startScale;
-    Vector2 startGlobalPosition;
-    float defaultYPos;
+	Vector2 startScale;
+	Vector2 startGlobalPosition;
+	float defaultYPos;
 
-    public CardConfig config;
-    float startRotation = -1.0f;
-    bool hasSetStartRotation = false;
+	public CardConfig config;
+	float startRotation = -1.0f;
+	bool hasSetStartRotation = false;
 
-    public PlayerState cardOwner;
+	public PlayerState cardOwner;
 
-    public static HashSet<CardComponent> hoveredCards = new HashSet<CardComponent>();
+	public static HashSet<CardComponent> hoveredCards = new HashSet<CardComponent>();
 
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-        startScale = GlobalScale;
-        defaultYPos = Position.Y;
-    }
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		startScale = GlobalScale;
+		defaultYPos = Position.Y;
+	}
 
-    public static CardComponent GetClosestHoveredCardToMouse(Viewport viewport)
-    {
-        float bestSquareDist = float.MaxValue;
-        CardComponent bestOption = null;
-        foreach (CardComponent cardComponent in hoveredCards)
-        {
-            float currSquareDist = (
-                viewport.GetMousePosition() - cardComponent.startGlobalPosition
-            ).LengthSquared();
-            if (currSquareDist < bestSquareDist)
-            {
-                bestSquareDist = currSquareDist;
-                bestOption = cardComponent;
-            }
-        }
-        return bestOption;
-    }
+	public static CardComponent GetClosestHoveredCardToMouse(Viewport viewport)
+	{
+		float bestSquareDist = float.MaxValue;
+		CardComponent bestOption = null;
+		foreach (CardComponent cardComponent in hoveredCards)
+		{
+			float currSquareDist = (
+				viewport.GetMousePosition() - cardComponent.startGlobalPosition
+			).LengthSquared();
+			if (currSquareDist < bestSquareDist)
+			{
+				bestSquareDist = currSquareDist;
+				bestOption = cardComponent;
+			}
+		}
+		return bestOption;
+	}
 
-    public override void _MouseEnter()
-    {
-        hoveredCards.Add(this);
-    }
+	public override void _MouseEnter()
+	{
+		hoveredCards.Add(this);
+	}
 
-    public override void _MouseExit()
-    {
-        hoveredCards.Remove(this);
-    }
+	public override void _MouseExit()
+	{
+		hoveredCards.Remove(this);
+	}
 
-    public bool IsMostHoveredCard()
-    {
-        return hoveredCards.Contains(this) && GetClosestHoveredCardToMouse(GetViewport()) == this;
-    }
+	public bool IsMostHoveredCard()
+	{
+		return hoveredCards.Contains(this) && GetClosestHoveredCardToMouse(GetViewport()) == this;
+	}
 
-    public override void _Process(double delta)
-    {
-        if (!hasSetStartRotation)
-        {
-            hasSetStartRotation = true;
-            startRotation = GlobalRotation;
-            startGlobalPosition = GlobalPosition;
-        }
-        if (IsMostHoveredCard())
-        {
-            ZIndex = 100;
+	public override void _Process(double delta)
+	{
+		if (!hasSetStartRotation)
+		{
+			hasSetStartRotation = true;
+			startRotation = GlobalRotation;
+			startGlobalPosition = GlobalPosition;
+		}
+		if (IsMostHoveredCard())
+		{
+			ZIndex = 100;
 
-            var tween = GetTree().CreateTween();
-            tween.TweenProperty(
-                this,
-                "global_scale",
-                new Vector2((startScale.X + 0.7f) / 2 * 1.3f, (startScale.Y + 0.7f) / 2 * 1.3f),
-                0.1f
-            );
-            tween.TweenProperty(this, "global_rotation", 0.0f, 0.1f);
-            tween.TweenProperty(this, "position", new Vector2(Position.X, defaultYPos - 100), 0.1f);
-        }
-        else
-        {
-            var tween = GetTree().CreateTween();
-            tween.TweenProperty(
-                this,
-                "global_scale",
-                new Vector2(startScale.X, startScale.Y),
-                0.1f
-            );
-            tween.TweenProperty(this, "global_rotation", startRotation, 0.1f);
-            tween.TweenProperty(this, "position", new Vector2(Position.X, defaultYPos), 0.1f);
-            ZIndex = (int)Mathf.Round(GlobalScale.X);
-        }
-    }
+			var tween = GetTree().CreateTween();
+			tween.TweenProperty(
+				this,
+				"global_scale",
+				new Vector2((startScale.X + 0.7f) / 2 * 1.3f, (startScale.Y + 0.7f) / 2 * 1.3f),
+				0.1f
+			);
+			tween.TweenProperty(this, "global_rotation", 0.0f, 0.1f);
+			tween.TweenProperty(this, "position", new Vector2(Position.X, defaultYPos - 100), 0.1f);
+		}
+		else
+		{
+			var tween = GetTree().CreateTween();
+			tween.TweenProperty(
+				this,
+				"global_scale",
+				new Vector2(startScale.X, startScale.Y),
+				0.1f
+			);
+			tween.TweenProperty(this, "global_rotation", startRotation, 0.1f);
+			tween.TweenProperty(this, "position", new Vector2(Position.X, defaultYPos), 0.1f);
+			ZIndex = (int)Mathf.Round(GlobalScale.X);
+		}
+	}
 
-    public override void _ExitTree()
-    {
-        hoveredCards.Remove(this);
-    }
+	public override void _ExitTree()
+	{
+		hoveredCards.Remove(this);
+	}
 
-    public override void _Input(InputEvent @event)
-    {
-        if (
-            @event is InputEventMouseButton
-            && ((InputEventMouseButton)@event).ButtonIndex == MouseButton.Left
-            && ((InputEventMouseButton)@event).IsPressed()
-            && IsMostHoveredCard()
-        )
-        {
-            if (RoundController.currentRoundState == RoundController.RoundState.playersTurn)
-            {
-                GetParent().Call("clicked_card", config);
-            }
-            else if (
-                RoundController.currentRoundState == RoundController.RoundState.selectACardToSteal
-            )
-            {
-                if (cardOwner == RoundController.instance.player)
-                {
-                    NotificationManager.ShowNotification("You can't steal your own card!");
-                }
-                else if (cardOwner != null)
-                {
-                    // steal card here
-                    RoundController.instance.player.cardsLaidDown.Add(config);
-                    cardOwner.cardsLaidDown.Remove(config);
-                    RoundController.RefreshAllVisuals();
-                    cardOwner.timesOffended += 1;
-                    PlayerState.PlayOffendedDialogue(cardOwner.player, cardOwner.timesOffended);
-                }
-            }
-        }
-    }
+	public override void _Input(InputEvent @event)
+	{
+		if (
+			@event is InputEventMouseButton
+			&& ((InputEventMouseButton)@event).ButtonIndex == MouseButton.Left
+			&& ((InputEventMouseButton)@event).IsPressed()
+			&& IsMostHoveredCard()
+		)
+		{
+			if (RoundController.currentRoundState == RoundController.RoundState.playersTurn)
+			{
+				GetParent().Call("clicked_card", config);
+			}
+			else if (
+				RoundController.currentRoundState == RoundController.RoundState.selectACardToSteal
+			)
+			{
+				if (cardOwner == RoundController.instance.player)
+				{
+					NotificationManager.ShowNotification("You can't steal your own card!");
+				}
+				else if (cardOwner != null)
+				{
+					// steal card here
+					RoundController.instance.player.cardsLaidDown.Add(config);
+					cardOwner.cardsLaidDown.Remove(config);
+					RoundController.RefreshAllVisuals();
+					cardOwner.timesOffended += 1;
+					PlayerState.PlayOffendedDialogue(cardOwner.player, cardOwner.timesOffended);
+				}
+			}
+		}
+	}
 }
--- src/RoundController.cs
+++ src/RoundController.cs
@@ -7,197 +7,197 @@ using System.Data;
 
 public partial class RoundController : Node
 {
-    [Export]
-    public PlayerState player;
+	[Export]
+	public PlayerState player;
 
-    [Export]
-    public Array<PlayerState> computers;
+	[Export]
+	public Array<PlayerState> computers;
 
-    [Export]
-    public CardListConfig cardList;
-    public static int CARDS_IN_HAND = 5;
+	[Export]
+	public CardListConfig cardList;
+	public static int CARDS_IN_HAND = 5;
 
-    [Export]
-    public static AudioStreamPlayer turnSfx;
+	[Export]
+	public static AudioStreamPlayer turnSfx;
 
-    public enum RoundState
-    {
-        playersTurn,
-        enemyTurn,
-        selectACardToSteal,
-        offendedDialogue,
-        roundEndWinnerDialogue,
-        RoundEndOffendedDialogue,
-    }
+	public enum RoundState
+	{
+		playersTurn,
+		enemyTurn,
+		selectACardToSteal,
+		offendedDialogue,
+		roundEndWinnerDialogue,
+		RoundEndOffendedDialogue,
+	}
 
-    public static int currentRound = 1;
+	public static int currentRound = 1;
 
-    public static RoundState currentRoundState = RoundState.playersTurn;
+	public static RoundState currentRoundState = RoundState.playersTurn;
 
-    public static void Reset()
-    {
-        currentRound = 0;
-        currentRoundState = RoundState.playersTurn;
-    }
+	public static void Reset()
+	{
+		currentRound = 0;
+		currentRoundState = RoundState.playersTurn;
+	}
 
-    public bool CanPlayerPlay()
-    {
-        return currentRoundState == RoundState.playersTurn;
-    }
+	public bool CanPlayerPlay()
+	{
+		return currentRoundState == RoundState.playersTurn;
+	}
 
-    public override void _Ready()
-    {
-        instance = this;
-        StartNewRound();
-    }
+	public override void _Ready()
+	{
+		instance = this;
+		StartNewRound();
+	}
 
-    public override void _Process(double delta)
-    {
-        UpdateState();
-        SetCurrentInstruction();
-    }
+	public override void _Process(double delta)
+	{
+		UpdateState();
+		SetCurrentInstruction();
+	}
 
-    public void SetCurrentInstruction()
-    {
-        if (currentRoundState == RoundState.selectACardToSteal)
-        {
-            CurrentInstructionManager.currentInstruction = "Select a card to steal";
-        }
-        else if (currentRoundState == RoundState.playersTurn)
-        {
-            CurrentInstructionManager.currentInstruction = "Click a card from your hand to play it";
-        }
-        else
-        {
-            CurrentInstructionManager.currentInstruction = "";
-        }
-    }
+	public void SetCurrentInstruction()
+	{
+		if (currentRoundState == RoundState.selectACardToSteal)
+		{
+			CurrentInstructionManager.currentInstruction = "Select a card to steal";
+		}
+		else if (currentRoundState == RoundState.playersTurn)
+		{
+			CurrentInstructionManager.currentInstruction = "Click a card from your hand to play it";
+		}
+		else
+		{
+			CurrentInstructionManager.currentInstruction = "";
+		}
+	}
 
-    public void UpdateState()
-    {
-        GD.Print(currentRoundState);
-        if (currentRoundState == RoundState.offendedDialogue && DialogueState.IsFinished())
-        {
-            RoundController.PlayComputers();
-        }
-        if (currentRoundState == RoundState.selectACardToSteal) { }
-        if (currentRoundState == RoundState.roundEndWinnerDialogue && DialogueState.IsFinished())
-        {
-            currentRoundState = RoundState.RoundEndOffendedDialogue;
-            DialogueState.SetDialogue(AngerBasedDialogue.GetDialog());
-        }
-        if (currentRoundState == RoundState.RoundEndOffendedDialogue && DialogueState.IsFinished())
-        {
-            StartNewRound();
-        }
-    }
+	public void UpdateState()
+	{
+		GD.Print(currentRoundState);
+		if (currentRoundState == RoundState.offendedDialogue && DialogueState.IsFinished())
+		{
+			RoundController.PlayComputers();
+		}
+		if (currentRoundState == RoundState.selectACardToSteal) { }
+		if (currentRoundState == RoundState.roundEndWinnerDialogue && DialogueState.IsFinished())
+		{
+			currentRoundState = RoundState.RoundEndOffendedDialogue;
+			DialogueState.SetDialogue(AngerBasedDialogue.GetDialog());
+		}
+		if (currentRoundState == RoundState.RoundEndOffendedDialogue && DialogueState.IsFinished())
+		{
+			StartNewRound();
+		}
+	}
 
-    public static RoundController instance;
+	public static RoundController instance;
 
-    public void StartNewRound()
-    {
-        Random random = new Random(DateTime.Now.Millisecond);
-        foreach (PlayerState computer in GetAllPlayers())
-        {
-            computer.cardsLaidDown = new Array<CardConfig>();
-            computer.cardsInHand = new Array<CardConfig>();
-            for (int i = 0; i < CARDS_IN_HAND; i++)
-            {
-                computer.cardsInHand.Add(cardList.cards[random.Next(cardList.cards.Count)]);
-            }
-            computer.RefreshVisuals();
-        }
-        currentRound += 1;
-        currentRoundState = RoundState.playersTurn;
-    }
+	public void StartNewRound()
+	{
+		Random random = new Random(DateTime.Now.Millisecond);
+		foreach (PlayerState computer in GetAllPlayers())
+		{
+			computer.cardsLaidDown = new Array<CardConfig>();
+			computer.cardsInHand = new Array<CardConfig>();
+			for (int i = 0; i < CARDS_IN_HAND; i++)
+			{
+				computer.cardsInHand.Add(cardList.cards[random.Next(cardList.cards.Count)]);
+			}
+			computer.RefreshVisuals();
+		}
+		currentRound += 1;
+		currentRoundState = RoundState.playersTurn;
+	}
 
-    public static void RefreshAllVisuals()
-    {
-        foreach (PlayerState playerState in RoundController.instance.GetAllPlayers())
-        {
-            playerState.RefreshVisuals();
-        }
-    }
+	public static void RefreshAllVisuals()
+	{
+		foreach (PlayerState playerState in RoundController.instance.GetAllPlayers())
+		{
+			playerState.RefreshVisuals();
+		}
+	}
 
-    public static void PlayComputers()
-    {
-        currentRoundState = RoundState.enemyTurn;
+	public static void PlayComputers()
+	{
+		currentRoundState = RoundState.enemyTurn;
 
-        Random random = new Random(DateTime.Now.Millisecond);
-        int index = 1;
+		Random random = new Random(DateTime.Now.Millisecond);
+		int index = 1;
 
-        foreach (PlayerState computer in instance.computers)
-        {
-            Timer nextTimer = new Timer();
-            nextTimer.WaitTime = 1.0f * index;
-            nextTimer.OneShot = true;
-            nextTimer.Timeout += () =>
-            {
-                int cardIndex = computer.cardsInHand.Count;
-                CardConfig computerCardToPlay = null;
-                foreach (CardConfig cardConfig in computer.cardsInHand)
-                {
-                    if (cardConfig.cardType == CardConfig.CardType.Jelly)
-                    {
-                        computerCardToPlay = cardConfig;
-                    }
-                }
-                if (computerCardToPlay == null)
-                {
-                    computerCardToPlay = computer.cardsInHand[random.Next(cardIndex)];
-                }
-                computer.PlayCard(computerCardToPlay, true);
-            };
-            computer.AddChild(nextTimer);
-            nextTimer.Start();
-            index += 1;
-        }
-        Timer canPlayTimer = new Timer();
-        canPlayTimer.WaitTime = 1.0f * (index) + 0.1f;
-        canPlayTimer.OneShot = true;
-        canPlayTimer.Timeout += () =>
-        {
-            currentRoundState = RoundState.playersTurn;
+		foreach (PlayerState computer in instance.computers)
+		{
+			Timer nextTimer = new Timer();
+			nextTimer.WaitTime = 1.0f * index;
+			nextTimer.OneShot = true;
+			nextTimer.Timeout += () =>
+			{
+				int cardIndex = computer.cardsInHand.Count;
+				CardConfig computerCardToPlay = null;
+				foreach (CardConfig cardConfig in computer.cardsInHand)
+				{
+					if (cardConfig.cardType == CardConfig.CardType.Jelly)
+					{
+						computerCardToPlay = cardConfig;
+					}
+				}
+				if (computerCardToPlay == null)
+				{
+					computerCardToPlay = computer.cardsInHand[random.Next(cardIndex)];
+				}
+				computer.PlayCard(computerCardToPlay, true);
+			};
+			computer.AddChild(nextTimer);
+			nextTimer.Start();
+			index += 1;
+		}
+		Timer canPlayTimer = new Timer();
+		canPlayTimer.WaitTime = 1.0f * (index) + 0.1f;
+		canPlayTimer.OneShot = true;
+		canPlayTimer.Timeout += () =>
+		{
+			currentRoundState = RoundState.playersTurn;
 
-            if (RoundController.instance.player.cardsInHand.Count == 0)
-            {
-                PlayRoundEndDialogue();
-            }
-            else
-            {
-                RoundController.RotateHands();
-            }
-            turnSfx?.Play(0);
-        };
-        instance.computers[1].AddChild(canPlayTimer);
-        canPlayTimer.Start();
-    }
+			if (RoundController.instance.player.cardsInHand.Count == 0)
+			{
+				PlayRoundEndDialogue();
+			}
+			else
+			{
+				RoundController.RotateHands();
+			}
+			turnSfx?.Play(0);
+		};
+		instance.computers[1].AddChild(canPlayTimer);
+		canPlayTimer.Start();
+	}
 
-    public List<PlayerState> GetAllPlayers()
-    {
-        var players = new List<PlayerState>(computers);
-        players.Add(player);
-        return players;
-    }
+	public List<PlayerState> GetAllPlayers()
+	{
+		var players = new List<PlayerState>(computers);
+		players.Add(player);
+		return players;
+	}
 
-    public static void PlayRoundEndDialogue()
-    {
-        currentRoundState = RoundState.roundEndWinnerDialogue;
-        Dialogue dialogue = WinnerDialogue.GetWinnerDialogue();
-        DialogueState.SetDialogue(dialogue);
-    }
+	public static void PlayRoundEndDialogue()
+	{
+		currentRoundState = RoundState.roundEndWinnerDialogue;
+		Dialogue dialogue = WinnerDialogue.GetWinnerDialogue();
+		DialogueState.SetDialogue(dialogue);
+	}
 
-    public static void RotateHands()
-    {
-        NotificationManager.ShowNotification("Passed hands clockwise");
-        List<PlayerState> players = RoundController.instance.GetAllPlayers();
+	public static void RotateHands()
+	{
+		NotificationManager.ShowNotification("Passed hands clockwise");
+		List<PlayerState> players = RoundController.instance.GetAllPlayers();
 
-        Array<CardConfig> firstHand = new Array<CardConfig>(players[0].cardsInHand);
-        players[0].cardsInHand = new Array<CardConfig>(players[1].cardsInHand);
-        players[1].cardsInHand = new Array<CardConfig>(players[2].cardsInHand);
-        players[2].cardsInHand = new Array<CardConfig>(players[3].cardsInHand);
-        players[3].cardsInHand = new Array<CardConfig>(firstHand);
+		Array<CardConfig> firstHand = new Array<CardConfig>(players[0].cardsInHand);
+		players[0].cardsInHand = new Array<CardConfig>(players[1].cardsInHand);
+		players[1].cardsInHand = new Array<CardConfig>(players[2].cardsInHand);
+		players[2].cardsInHand = new Array<CardConfig>(players[3].cardsInHand);
+		players[3].cardsInHand = new Array<CardConfig>(firstHand);
 
-        RefreshAllVisuals();
-    }
+		RefreshAllVisuals();
+	}
 }
