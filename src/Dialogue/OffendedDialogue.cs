c392a8b3b99dcf6d01f168f247048442766ec8be
wadlo <jalen.morgan000@gmail.com>
2023-11-25 22:09:59 -0500

    Add the rest of the offended dialogues

--- src/Dialogue/OffendedDialogue.cs
+++ src/Dialogue/OffendedDialogue.cs
@@ -28,6 +28,165 @@ public class OffendedDialogue
             )
         };
 
+        if (offendedPlayer == Dialogue.players.Dwarf)
+        {
+            if (angerLevel == 1)
+            {
+                possibleDialogues = new List<Dialogue>()
+                {
+                    new Dialogue(
+                        new List<DialogueLine>() { new DialogueLine(Dialogue.players.Dwarf, "…"), }
+                    ),
+                    new Dialogue(
+                        new List<DialogueLine>()
+                        {
+                            new DialogueLine(Dialogue.players.Dwarf, "Hm."),
+                        }
+                    ),
+                    new Dialogue(
+                        new List<DialogueLine>()
+                        {
+                            new DialogueLine(Dialogue.players.Dwarf, "Nice."),
+                        }
+                    ),
+                };
+            }
+
+            if (angerLevel == 2)
+            {
+                possibleDialogues = new List<Dialogue>()
+                {
+                    new Dialogue(
+                        new List<DialogueLine>()
+                        {
+                            new DialogueLine(
+                                Dialogue.players.Dwarf,
+                                "Keep yer cards on yer side’a the table."
+                            ),
+                        }
+                    ),
+                    new Dialogue(
+                        new List<DialogueLine>()
+                        {
+                            new DialogueLine(Dialogue.players.Dwarf, "Yer real fun, aren’tcha?"),
+                        }
+                    ),
+                    new Dialogue(
+                        new List<DialogueLine>()
+                        {
+                            new DialogueLine(Dialogue.players.Dwarf, "Try that again."),
+                        }
+                    ),
+                };
+            }
+
+            if (angerLevel == 3)
+            {
+                possibleDialogues = new List<Dialogue>()
+                {
+                    new Dialogue(
+                        new List<DialogueLine>()
+                        {
+                            new DialogueLine(
+                                Dialogue.players.Dwarf,
+                                "And ya wonder why we play without ya.."
+                            ),
+                        }
+                    ),
+                    new Dialogue(
+                        new List<DialogueLine>()
+                        {
+                            new DialogueLine(Dialogue.players.Dwarf, "Do ya ever have fun?"),
+                        }
+                    ),
+                    new Dialogue(
+                        new List<DialogueLine>()
+                        {
+                            new DialogueLine(
+                                Dialogue.players.Dwarf,
+                                "Don’t think about coming back after tonight."
+                            ),
+                        }
+                    ),
+                };
+            }
+        }
+
+        if (offendedPlayer == Dialogue.players.Golem)
+        {
+            if (angerLevel == 1)
+            {
+                possibleDialogues = new List<Dialogue>()
+                {
+                    new Dialogue(
+                        new List<DialogueLine>()
+                        {
+                            new DialogueLine(Dialogue.players.Golem, "You Touched My Cards!"),
+                        }
+                    ),
+                    new Dialogue(
+                        new List<DialogueLine>()
+                        {
+                            new DialogueLine(Dialogue.players.Golem, "That Is Fun."),
+                        }
+                    ),
+                    new Dialogue(
+                        new List<DialogueLine>() { new DialogueLine(Dialogue.players.Golem, ":D"), }
+                    ),
+                };
+            }
+
+            if (angerLevel == 2)
+            {
+                possibleDialogues = new List<Dialogue>()
+                {
+                    new Dialogue(
+                        new List<DialogueLine>()
+                        {
+                            new DialogueLine(Dialogue.players.Golem, "What Is This?"),
+                        }
+                    ),
+                    new Dialogue(
+                        new List<DialogueLine>()
+                        {
+                            new DialogueLine(Dialogue.players.Golem, "What Are You Doing?"),
+                        }
+                    ),
+                    new Dialogue(
+                        new List<DialogueLine>()
+                        {
+                            new DialogueLine(Dialogue.players.Golem, "That Is A Strange Choice."),
+                        }
+                    ),
+                };
+            }
+
+            if (angerLevel == 3)
+            {
+                possibleDialogues = new List<Dialogue>()
+                {
+                    new Dialogue(
+                        new List<DialogueLine>()
+                        {
+                            new DialogueLine(Dialogue.players.Golem, "This Isn’t Fun."),
+                        }
+                    ),
+                    new Dialogue(
+                        new List<DialogueLine>()
+                        {
+                            new DialogueLine(
+                                Dialogue.players.Golem,
+                                "I Don’t Want To Play Anymore."
+                            ),
+                        }
+                    ),
+                    new Dialogue(
+                        new List<DialogueLine>() { new DialogueLine(Dialogue.players.Golem, "):"), }
+                    ),
+                };
+            }
+        }
+
         if (offendedPlayer == Dialogue.players.Elf)
         {
             if (angerLevel == 1)
