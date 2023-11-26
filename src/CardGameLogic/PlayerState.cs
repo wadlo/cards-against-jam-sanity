0f75be363224b22df4427dd7469494da79440e9e
wadlo <jalen.morgan000@gmail.com>
2023-11-26 13:21:02 -0500

    Show notifications

--- src/CardGameLogic/PlayerState.cs
+++ src/CardGameLogic/PlayerState.cs
@@ -6,98 +6,102 @@ using System.Runtime.CompilerServices;
 
 public partial class PlayerState : Node
 {
-	public Array<CardConfig> cardsLaidDown = new Array<CardConfig>();
+    public Array<CardConfig> cardsLaidDown = new Array<CardConfig>();
 
-	[Export]
-	public string name;
+    [Export]
+    public string name;
 
-	[Export]
-	public Dialogue.players player;
+    [Export]
+    public Dialogue.players player;
 
-	[Export]
-	public Array<CardConfig> cardsInHand;
+    [Export]
+    public Array<CardConfig> cardsInHand;
 
-	// Each time the player makes a bad choice, friendship goes down. When a good choice is made, it goes up.
-	public int timesOffended = 0;
+    // Each time the player makes a bad choice, friendship goes down. When a good choice is made, it goes up.
+    public int timesOffended = 0;
 
-	[Export]
-	public int timesOffendedBeforeQuitting = 5;
+    [Export]
+    public int timesOffendedBeforeQuitting = 5;
 
-	[Export]
-	public Node2D handVisuals;
+    [Export]
+    public Node2D handVisuals;
 
-	[Export]
-	public PackedScene cardPackedScene;
+    [Export]
+    public PackedScene cardPackedScene;
 
-	public Action visualsRefreshed = () => { };
+    public Action visualsRefreshed = () => { };
 
-	[Export]
-	public bool isPlayer;
+    [Export]
+    public bool isPlayer;
 
-	public override void _Ready()
-	{
-		if (handVisuals != null)
-			RefreshVisuals();
-	}
+    public override void _Ready()
+    {
+        if (handVisuals != null)
+            RefreshVisuals();
+    }
 
-	public void RefreshVisuals()
-	{
-		if (handVisuals != null)
-		{
-			foreach (Node node in handVisuals.GetChildren())
-			{
-				node.QueueFree();
-			}
-			foreach (CardConfig card in cardsInHand)
-			{
-				Node2D instantiatedCard = (Node2D)cardPackedScene.Instantiate();
-				instantiatedCard.Set("config", card);
-				handVisuals.AddChild(instantiatedCard);
-				instantiatedCard.GetChild<Sprite2D>(0).Texture = card.cardImage;
-			}
-		}
+    public void RefreshVisuals()
+    {
+        if (handVisuals != null)
+        {
+            foreach (Node node in handVisuals.GetChildren())
+            {
+                node.QueueFree();
+            }
+            foreach (CardConfig card in cardsInHand)
+            {
+                Node2D instantiatedCard = (Node2D)cardPackedScene.Instantiate();
+                instantiatedCard.Set("config", card);
+                handVisuals.AddChild(instantiatedCard);
+                instantiatedCard.GetChild<Sprite2D>(0).Texture = card.cardImage;
+            }
+        }
 
-		visualsRefreshed.Invoke();
-	}
+        visualsRefreshed.Invoke();
+    }
 
-	public void PlayCard(CardConfig clickedCard, bool isAutomatic = false)
-	{
-		GetNode<AudioStreamPlayer>("../../CardSFX").Play(0);
-		cardsLaidDown.Add(clickedCard);
-		cardsInHand.Remove(clickedCard);
-		RefreshVisuals();
-		if (isAutomatic == false)
-		{
-			if (clickedCard.cardType == CardConfig.CardType.Honey)
-			{
-				PickACardToSteal();
-			}
-			else
-			{
-				RoundController.PlayComputers();
-			}
-		}
-	}
+    public void PlayCard(CardConfig clickedCard, bool isAutomatic = false)
+    {
+        GetNode<AudioStreamPlayer>("../../CardSFX").Play(0);
+        cardsLaidDown.Add(clickedCard);
+        cardsInHand.Remove(clickedCard);
+        RefreshVisuals();
+        if (isAutomatic == false)
+        {
+            if (clickedCard.cardType == CardConfig.CardType.Honey)
+            {
+                PickACardToSteal();
+            }
+            else
+            {
+                RoundController.PlayComputers();
+            }
+        }
+    }
 
-	public static void PickACardToSteal()
-	{
-		RoundController.currentRoundState = RoundController.RoundState.selectACardToSteal;
-		if (RoundController.instance.player.cardsInHand.Count == RoundController.CARDS_IN_HAND - 1)
-		{
-			RoundController.currentRoundState = RoundController.RoundState.offendedDialogue;
-			RoundController.RefreshAllVisuals();
-			NotificationManager.ShowNotification("There are no cards to steal");
-		}
-	}
+    public static void PickACardToSteal()
+    {
+        RoundController.currentRoundState = RoundController.RoundState.selectACardToSteal;
+        if (RoundController.instance.player.cardsInHand.Count == RoundController.CARDS_IN_HAND - 1)
+        {
+            RoundController.currentRoundState = RoundController.RoundState.offendedDialogue;
+            RoundController.RefreshAllVisuals();
+            NotificationManager.ShowNotification("There are no cards to steal");
+        }
+        else
+        {
+            NotificationManager.ShowNotification("Select a card to steal");
+        }
+    }
 
-	public static void PlayOffendedDialogue(Dialogue.players offendedPlayer, int offendedLevel)
-	{
-		RoundController.currentRoundState = RoundController.RoundState.offendedDialogue;
+    public static void PlayOffendedDialogue(Dialogue.players offendedPlayer, int offendedLevel)
+    {
+        RoundController.currentRoundState = RoundController.RoundState.offendedDialogue;
 
-		Dialogue dialogue = OffendedDialogue.GetRandomDialogueForOffendedPlayer(
-			offendedPlayer,
-			offendedLevel
-		);
-		DialogueState.SetDialogue(dialogue);
-	}
+        Dialogue dialogue = OffendedDialogue.GetRandomDialogueForOffendedPlayer(
+            offendedPlayer,
+            offendedLevel
+        );
+        DialogueState.SetDialogue(dialogue);
+    }
 }
--- src/NotificationManager.cs
+++ src/NotificationManager.cs
@@ -1,9 +1,24 @@
 using Godot;
 
-public class NotificationManager
+public partial class NotificationManager : ColorRect
 {
+    float opacity = 0.0f;
+    public static NotificationManager instance;
+
+    public override void _Ready()
+    {
+        instance = this;
+    }
+
     public static void ShowNotification(string notification)
     {
-        GD.Print(notification);
+        instance.opacity = 2.0f;
+        instance.GetChild<Label>(0).Text = notification;
+    }
+
+    public override void _Process(double delta)
+    {
+        opacity -= (float)delta;
+        Modulate = new Color(1, 1, 1, Mathf.Max(0.0f, Mathf.Min(1.0f, opacity)));
     }
 }
